581af7670fde3e5312d73fbbfba008c5bd04345e
Ashekuchiha <ashakulislam0000@gmail.com>
2023-07-19 01:48:39 +0600

    url and path nia alchona

--- project1/index.js
+++ project1/index.js
@@ -1,5 +1,8 @@
 //dependencies
 const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
+
 
 //app obj
 const app={};
@@ -19,7 +22,31 @@ app.createServer=()=>{
 
 //handel req ress
 app.handeleReq=(req,ress)=>{
-    ress.end("hooooooinecrhh g hdf jdgk");
+    const parseUrl=url.parse(req.url,true);
+      //console.log(parseUrl);
+    const path = parseUrl.pathname;
+    //console.log(path);
+    const treamedPath=path.replace(/^\+|\/+$/g,"");
+    //console.log(treamedPath);
+    const method = req.method.toLowerCase();
+    //console.log(method);
+    const querryStringObject= parseUrl.query;
+    //console.log(querryStringObject);
+    const headersObj=req.headers;
+    //console.log(headersObj);
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+    req.on('data',(buffer)=>{
+        realData +=decoder.write(buffer);
+    });
+    req.on('end',()=>{
+        realData +=decoder.end();
+        console.log(realData);
+        ress.end("hooooooinecrhh g hdf jdgk");
+
+    });
+
 }
 
 app.createServer();
